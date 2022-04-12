eb63c8854b13eb760752a600ea46b497394ebeab
Jack Hengen <jackhengen@gmail.com>
2022-04-12 18:53:37 -0400

    added updateNavigation, removed notes.txt

---
+++ updateNavigation.js
@@ -0,0 +1,4 @@
+"use strict"
+navContent = `<a href="index.html">Main</a> | <a href="projects.html>Projects</a>`;
+const navDiv =document.getElementById("navigationDiv");
+navDiv.innerHTML = navContent;
