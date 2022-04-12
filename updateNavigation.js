985c53f1b3b3aa61eb79d8c00d8240bc467139e5
Jack Hengen <jackhengen@gmail.com>
2022-04-12 18:58:13 -0400

    fixed bug with updateNavigation.js

--- updateNavigation.js
+++ updateNavigation.js
@@ -1,4 +1,4 @@
 "use strict"
-navContent = `<a href="index.html">Main</a> | <a href="projects.html>Projects</a>`;
+const navContent = `<a href="index.html">Main</a> | <a href="projects.html>Projects</a>`;
 const navDiv =document.getElementById("navigationDiv");
 navDiv.innerHTML = navContent;
