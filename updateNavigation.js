a484143901b83d628fcd07b34d5e2630a234ab1a
Jack Hengen <81589257+JackHengen@users.noreply.github.com>
2022-04-12 19:17:01 -0400

    Update updateNavigation.js

--- updateNavigation.js
+++ updateNavigation.js
@@ -1,4 +1,4 @@
 "use strict"
-const navContent = `<a href="index.html">Main</a><p> | </p><a href="projects.html>Projects</a>`;
+const navContent = `<a href="index.html">Main</a><p> | </p><a href="projects.html">Projects</a>`;
 const navDiv =document.getElementById("navigationDiv");
 navDiv.innerHTML = navContent;
